47821439472041f80da18bdd1b94f3b0ba20e062
VictorSmits <victor-smi@hotmail.fr>
2024-11-10 15:36:29 +0100

    update to js

--- src/server/index.js
+++ src/server/index.js
@@ -15,56 +15,49 @@ const redis = new Redis({
 
 const CacheArray = {
 
-  redis,
-
-  constructor(redis) {
-    this.redis = redis;
-  },
-
   add(key, value) {
-    return this.redis.call('JSON.ARRAPPEND', key, '$', JSON.stringify(value));
+    return redis.call('JSON.ARRAPPEND', key, '$', JSON.stringify(value));
   },
 
   new(key) {
-    return this.redis.call('JSON.SET', key, '$', JSON.stringify([]));
+    return redis.call('JSON.SET', key, '$', JSON.stringify([]));
   },
 
   delete(key, index) {
-    return this.redis.call('JSON.DEL', key, `$[${index}]`);
+    return redis.call('JSON.DEL', key, `$[${index}]`);
   },
 
   read(key, index) {
-    return this.redis.call('JSON.GET', key, `$[${index}]`);
+    return redis.call('JSON.GET', key, `$[${index}]`);
   },
 
   readAll(key) {
-    return this.redis.call('JSON.GET', key, `$`);
+    return redis.call('JSON.GET', key, `$`);
   },
 
   setKey(key, index, objectKey, value) {
-    return this.redis.call('JSON.SET', key, `$[${index}].^${objectKey}`, value);
+    return redis.call('JSON.SET', key, `$[${index}].^${objectKey}`, value);
   },
 
   set(key, index, value) {
-    return this.redis.call('JSON.SET', key, `$[${index}]`,
+    return redis.call('JSON.SET', key, `$[${index}]`,
       JSON.stringify(value));
   }
 };
 
 const Cache = {
-  redis,
 
   array: CacheArray,
 
   add(key, value, expires) {
     if (expires) {
-      return this.redis.set(key, value, 'EX', expires);
+      return redis.set(key, value, 'EX', expires);
     }
-    return this.redis.set(key, value);
+    return redis.set(key, value);
   },
 
   async read(key, cb) {
-    const data = await this.get(key);
+    const data = await redis.get(key);
     Callback(cb, data);
   }
 
