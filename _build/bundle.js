4b1c7cf3c682b6ab1c799122203db6a9dad967b8
VictorSmits <victor-smi@hotmail.fr>
2024-11-10 14:44:09 +0100

    update build

--- _build/bundle.js
+++ _build/bundle.js
@@ -1,56 +1,24 @@
-const esbuild = require("esbuild");
+//@ts-check
+import { exists, exec, getFiles } from './utils.js';
+import { createBuilder, createFxmanifest } from '@overextended/fx-utils';
 
-const IS_WATCH_MODE = process.env.IS_WATCH_MODE;
+const watch = process.argv.includes('--watch');
 
-const TARGET_ENTRIES = [
+createBuilder(
+  watch,
   {
-    target: "node18",
-    entryPoints: ["server/index.ts"],
-    platform: "node",
-    outfile: "./dist/server/server.js",
+    dropLabels: !watch ? ['DEV'] : undefined,
   },
-];
-
-const buildBundle = async () => {
-  try {
-    const baseOptions = {
-      logLevel: "info",
-      bundle: true,
-      charset: "utf8",
-      minifyWhitespace: true,
-      absWorkingDir: process.cwd(),
-    };
-
-    for (const targetOpts of TARGET_ENTRIES) {
-      const mergedOpts = { ...baseOptions, ...targetOpts };
-
-      if (IS_WATCH_MODE) {
-        mergedOpts.watch = {
-          onRebuild(error) {
-            if (error)
-              console.error(
-                `[ESBuild Watch] (${targetOpts.entryPoints[0]}) Failed to rebuild bundle`
-              );
-            else
-              console.log(
-                `[ESBuild Watch] (${targetOpts.entryPoints[0]}) Sucessfully rebuilt bundle`
-              );
-          },
-        };
-      }
-
-      const { errors } = await esbuild.build(mergedOpts);
-
-      if (errors.length) {
-        console.error(`[ESBuild] Bundle failed with ${errors.length} errors`);
-        process.exit(1);
-      }
-    }
-  } catch (e) {
-    console.log("[ESBuild] Build failed with error");
-    console.error(e);
-    process.exit(1);
+  [
+    {
+      name: 'server',
+      options: {
+        platform: 'node',
+        target: ['node16'],
+        format: 'cjs',
+      },
+    },
+  ],
+  async (outfiles) => {
   }
-};
-
-buildBundle().catch(() => process.exit(1));
+);
