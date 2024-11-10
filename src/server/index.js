91cce4b9eb0a58aa0932eefa7a39c2073f294ef3
VictorSmits <victor-smi@hotmail.fr>
2024-11-10 15:30:53 +0100

    update to js

--- src/server/index.js
+++ src/server/index.js
@@ -1,76 +1,74 @@
 const Redis = require('ioredis');
-const redis_host = GetConvar("redis_host", "127.0.0.1");
+const redis_host = GetConvar('redis_host', '127.0.0.1');
 
 function Callback(cb, ...args) {
   if (typeof cb === 'function') return setImmediate(() => cb(...args));
   else return false;
 }
 
-class CacheArray {
+const redis = new Redis({
+  port: 6379,
+  host: redis_host,
+  family: 4,
+  db: 0
+});
 
-  redis;
+const CacheArray = {
+
+  redis,
 
   constructor(redis) {
     this.redis = redis;
-  }
+  },
 
   add(key, value) {
     return this.redis.call('JSON.ARRAPPEND', key, '$', JSON.stringify(value));
-  }
+  },
 
   new(key) {
     return this.redis.call('JSON.SET', key, '$', JSON.stringify([]));
-  }
+  },
 
   delete(key, index) {
     return this.redis.call('JSON.DEL', key, `$[${index}]`);
-  }
+  },
 
   read(key, index) {
     return this.redis.call('JSON.GET', key, `$[${index}]`);
-  }
+  },
 
   readAll(key) {
     return this.redis.call('JSON.GET', key, `$`);
-  }
+  },
 
   setKey(key, index, objectKey, value) {
     return this.redis.call('JSON.SET', key, `$[${index}].^${objectKey}`, value);
-  }
+  },
 
   set(key, index, value) {
     return this.redis.call('JSON.SET', key, `$[${index}]`,
       JSON.stringify(value));
   }
-}
+};
 
+const Cache = {
+  redis,
 
-class Cache extends Redis {
-  array
-
-  constructor() {
-    super({
-      port: 6379,
-      host: redis_host,
-      family: 4,
-      db: 0,
-    })
-    this.array = new CacheArray(this)
-  }
+  array: CacheArray,
 
   add(key, value, expires) {
     if (expires) {
-      return this.set(key, value, 'EX', expires);
+      return this.redis.set(key, value, 'EX', expires);
     }
-    return this.set(key, value);
-  }
+    return this.redis.set(key, value);
+  },
 
   async read(key, cb) {
     const data = await this.get(key);
     Callback(cb, data);
   }
 
-}
+};
 exports('GetInterface', () => {
-  return new Cache();
+  return Cache;
 });
