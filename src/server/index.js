e2b825e976bacfc90ddf51e1221e3c36202ee638
VictorSmits <victor-smi@hotmail.fr>
2024-11-10 15:21:15 +0100

    update to js

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,49 @@
 const Redis = require('ioredis');
-const { CacheArray } = require('./array');
-
 const redis_host = GetConvar("redis_host", "127.0.0.1");
 
 function Callback(cb, ...args) {
   if (typeof cb === 'function') return setImmediate(() => cb(...args));
   else return false;
 }
+
+class CacheArray {
+
+  redis;
+
+  constructor(redis) {
+    this.redis = redis;
+  }
+
+  add(key, value) {
+    return this.redis.call('JSON.ARRAPPEND', key, '$', JSON.stringify(value));
+  }
+
+  new(key) {
+    return this.redis.call('JSON.SET', key, '$', JSON.stringify([]));
+  }
+
+  delete(key, index) {
+    return this.redis.call('JSON.DEL', key, `$[${index}]`);
+  }
+
+  read(key, index) {
+    return this.redis.call('JSON.GET', key, `$[${index}]`);
+  }
+
+  readAll(key) {
+    return this.redis.call('JSON.GET', key, `$`);
+  }
+
+  setKey(key, index, objectKey, value) {
+    return this.redis.call('JSON.SET', key, `$[${index}].^${objectKey}`, value);
+  }
+
+  set(key, index, value) {
+    return this.redis.call('JSON.SET', key, `$[${index}]`,
+      JSON.stringify(value));
+  }
+}
+
 
 class Cache extends Redis {
   array
@@ -34,12 +71,6 @@ class Cache extends Redis {
   }
 
 }
-
-module.exports = {
-  Cache,
-  Callback
-};
-
 exports('GetInterface', () => {
   return new Cache();
 });
